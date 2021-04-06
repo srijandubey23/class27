56bb25969c8d72dd641dcba473ee13d1af3b3594
srijandubey23 <srijandubey639@gmail.com>
2021-04-06 17:09:18 +0530

    constraint added

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var slingshot;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -32,18 +34,19 @@ function setup(){
 
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
+    log5 = new Log(850,120,150, -PI/7);
+    log6=new Log(230,180,80,PI/2);
     bird = new Bird(100,100);
+    slingshot = new Slingshot(bird.body,log6.body);
 
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    //console.log(box2.body.position.x);
+    //console.log(box2.body.position.y);
+   // console.log(box2.body.angle);
     box1.display();
     box2.display();
     ground.display();
@@ -58,7 +61,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    slingshot.display();
     bird.display();
+    log6.display();
+    
     platform.display();
 }
